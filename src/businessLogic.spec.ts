9a8f450555a1e78a4ac5f56fa265acacedfcfcc9
Emma Mason <emma.mason@leanix.net>
2023-07-12 12:15:12 -0400

    Current state

--- src/businessLogic.spec.ts
+++ src/businessLogic.spec.ts
@@ -1,25 +1,9 @@
-import { Authenticator, GraphQLClient } from 'leanix-js'
-import { fetchApplications, mutateApplications } from './businessLogic'
+import { sum } from './businessLogic'
 
-const host = process.env.LEANIX_HOST
-const apitoken = process.env.LEANIX_APITOKEN
-
-describe('The business logic', () => {
-  const authenticator = new Authenticator({ host, apitoken })
-  const graphql = new GraphQLClient(authenticator)
-  beforeAll(async () => {
-    expect(typeof host).toEqual('string')
-    expect(typeof apitoken).toEqual('string')
-    await authenticator.start()
-  })
-  afterAll(() => {
-    authenticator.stop()
-  })
-
-  it('fetches applications from the inventory', async () => {
-    const applications = await fetchApplications({ graphql })
-    expect(Array.isArray(applications)).toBeTruthy()
-    const updatedApplications = await mutateApplications({ applications, graphql })
-    expect(Array.isArray(updatedApplications)).toBeTruthy()
-  }, 10000)
+// JEST, testing library
+it('computes the sum of two numbers', async () => {
+  const a = 1
+  const b = 2
+  const c = sum(1, 2)
+  expect(a + b).toEqual(c)
 })
